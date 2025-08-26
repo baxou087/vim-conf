1d4ddd57dece35c39852f9357becd82f42abbe78
baxou087 <vassili.samios@gmail.com>
2025-08-26 15:35:32 +0200

    Update init.lua
    adjustments and added Gitsigns.nvim

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,14 +1,4 @@
 
---vim.g.loaded_python3_provider = 0
---vim.g.loaded_ruby_provider = 0
---vim.g.loaded_perl_provider = 0
---vim.g.loaded_node_provider = 0
-
-
-
-
-
-require('user.theme')
 
 -- Initialize core settings first
 require('user.options')
@@ -21,7 +11,9 @@ require('user.treesitter') -- Set up before LSP for better highlighting
 require('user.lsp')  -- Depends on language servers being available
 require('user.completion') -- Depends on LSP configuration
 require('user.telescope') -- Often integrates with LSP
+require('user.gitsigns') -- git integration
 
+require('user.theme')
 
 
 
