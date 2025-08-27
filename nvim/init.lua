75aaa350fdd32e700019d57d25b4095a3d4f7968
baxou087 <vassili.samios@gmail.com>
2025-08-27 13:54:05 +0200

    Update init.lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -16,14 +16,4 @@ require('user.aerial') -- aerial
 require('user.codedocs') -- documentation generator
 require('user.cursorword')
 require('user.theme')
-
-
-
-
-
-
-
-
-
-
-
+require('user.hardline')
