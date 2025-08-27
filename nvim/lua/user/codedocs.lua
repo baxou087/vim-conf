b0148fa15084113d765cb7c3709afe55fea4873f
baxou087 <vassili.samios@gmail.com>
2025-08-27 13:05:24 +0200

    Update codedocs.lua
    added c lang

--- nvim/lua/user/codedocs.lua
+++ nvim/lua/user/codedocs.lua
@@ -1,5 +1,8 @@
 require("codedocs").setup {
-    default_styles = {python = "Google"}
+    default_styles = {
+        python = "Google",
+        c = "Doxygen",
+    }
 }
 
 vim.keymap.set("n", "<leader>c", "<cmd>Codedocs<CR>")
