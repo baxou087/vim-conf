9f1758d12377432e9ed283a1ec87a6be0cb9e60b
baxou087 <vassili.samios@gmail.com>
2025-09-03 07:49:26 +0200

    Update completion.lua

--- nvim/lua/user/completion.lua
+++ nvim/lua/user/completion.lua
@@ -75,3 +75,6 @@ local capabilities = require('cmp_nvim_lsp').default_capabilities()
 require('lspconfig')['pyright'].setup {
   capabilities = capabilities
 }
+require('lspconfig')['clangd'].setup {
+  capabilities = capabilities
+}
